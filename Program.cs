7199856c2dc8d655c3dacef632ecb650f7eff421
chaichico <chaiwet_ke@cmu.ac.th>
2026-04-21 16:06:47 +0700

    build: configure database connection and Docker environment
    - Update connection string to use environment variables
    - Configure Docker Compose with SQL Server 2022
    - Add environment variables for database credentials
    - Remove .env.example file
    - Update appsettings for Docker deployment

--- Program.cs
+++ Program.cs
@@ -1,25 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Data;
 using Controllers;
-using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Load .env file
-Env.Load();
-
-// Build Connection String from Environment Variables
-var dbServer = Environment.GetEnvironmentVariable("DB_SERVER") ?? "localhost";
-var dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "1433";
-var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "EcommerceDb";
-var dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "sa";
-var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "YourStrong!Pass123";
-
-var connectionString = $"Server={dbServer},{dbPort};Database={dbName};User={dbUser};Password={dbPassword};TrustServerCertificate=True;";
-
 // DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(connectionString)
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 // Controller
